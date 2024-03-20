ed656b4fb48d60bd992e01f565fe59722fd322a7
FahithKRM <FahithKRM>
2024-03-20 23:16:04 +0530

    Update

---
+++ Registration/frontend/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import LoginSignUp from './Components/LoginSignUp/LoginSignUp.jsx';
+
+function App() {
+  return (
+    <div>
+      <LoginSignUp />
+    </div>
+  );
+}
+
+export default App;
